e71c6d5c08bbf6e38c1b9bbe6c5b1b4d8377e8bd
komandotayfa <53448093+komandotayfa@users.noreply.github.com>
2024-02-05 20:16:06 +0300

    code simplifying

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-An app that remind me to get up
+1 saat oturunca bana kalkmamı sağlayan program
+
+A program that remind me to get up after 1 hour of sitting
--- getup.py
+++ getup.py
@@ -2,31 +2,23 @@ import time
 import tkinter as tk
 from plyer import notification
 
-def create_gui():
-    # Create the Tkinter window
+def gui():
     root = tk.Tk()
     root.title("KAPAT")
-
-    # Create a label
     label = tk.Label(root, text="KAPAT ŞUNU!")
-
-    # Pack the label and button into the window
     label.pack(pady=10)
-
-    # Start the Tkinter event loop
     root.mainloop()
 
-def remind_to_get_up():
+def getup():
     notification.notify(
         title='KALK!',
         message='KALK LAN!',
-        timeout=20  # The notification will disappear after 10 seconds
+        timeout=20  # Saniye/Seconds
     )
 
 if __name__ == "__main__":
-    # Set the interval to 1 hour (3600 seconds)
-    interval_seconds = 3600
+    waitsec = 3600  # Saniye/Seconds
     while True:
-            time.sleep(interval_seconds)  # Sleep for the specified interval
-            remind_to_get_up()
-            create_gui()
+            time.sleep(waitsec)
+            getup()
+            gui()
